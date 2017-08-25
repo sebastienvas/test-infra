628f84cc7a55b3c2ace9794e334cceaea7d131de
Sebastien Vas <sebastienvas@google.com>
2017-08-24 21:57:43 -0700

    WIP

---
+++ toolbox/clusterpool/poolmanager/queue.go
@@ -0,0 +1,14 @@
+package poolmanager
+
+
+type PoolQueue struct {
+	size int
+	configHash string
+}
+
+
+func (q *PoolQueue) Add()
+func (q *PoolQueue) New()
+func (q *PoolQueue) Delete()
+func (q *PoolQueue) Get()
+func (q *PoolQueue) ()
